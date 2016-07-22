fd7c7068f6e4305199050a93e11202eeeb2e173c
markmwaura <markkmwaura@gmail.com>
2016-07-22 10:58:40 +0300

    add javascript for mainbody

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-$(document).foundation()
+function focusOnElement(element_id) {
+
+    goToByScroll(element_id);
+}
+function goToByScroll(id){
+
+    $('html,body').animate({
+        scrollTop: $("#"+id).offset().top},
+        'slow');
+}
