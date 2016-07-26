7bf9a173134c8b0e05f4c23ec3d187d04388134c
markmwaura <markkmwaura@gmail.com>
2016-07-26 23:03:30 +0300

    add proper menu javascript

--- js/app.js
+++ js/app.js
@@ -8,3 +8,24 @@ function goToByScroll(id){
         scrollTop: $("#"+id).offset().top},
         'slow');
 }
+
+$(document).ready(function() {
+  //change the integers below to match the height of your upper dive, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () {
+
+    console.log($(window).scrollTop());
+
+    if ($(window).scrollTop() > 550) {
+
+      $('.leftnav').addClass('leftnav-fixed');
+    }
+
+    if ($(window).scrollTop() < 551) {
+      $('.leftnav').removeClass('leftnav-fixed');
+    }
+  });
+});
